4e72eedbbc7d9360bd08662fe73a090c09dbc34b
Sivakumar S <sivakumarit42@gmail.com>
2023-03-27 10:39:57 +0530

    changes

--- main.tf
+++ main.tf
@@ -1,28 +1,3 @@
-#resource "aws_elasticache_cluster" "elasticache" {
-#  cluster_id        = "${var.env}-elasticache"
-#  engine            = var.engine
-#  node_type         = var.node_type
-#  num_cache_nodes   = var.num_cache_nodes
-#  engine_version    = var.engine_version
-#  port              = 6379
-#  subnet_group_name = aws_elasticache_subnet_group.main.name
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "${var.env}-elasticache" }
-#  )
-#}
-#
-#resource "aws_elasticache_subnet_group" "main" {
-#  name       = "${var.env}-rds"
-#  subnet_ids = var.subnet_ids
-#
-#  tags = merge(
-#    var.tags,
-#    { Name = "${var.env}-subnet-group" }
-#  )
-#}
-
 resource "aws_elasticache_cluster" "elasticache" {
   cluster_id        = "${var.env}-elasticache"
   engine            = var.engine
@@ -48,12 +23,37 @@ resource "aws_elasticache_subnet_group" "main" {
   )
 }
 
-output "redis" {
-  value = aws_elasticache_cluster.elasticache
-}
-
-resource "aws_ssm_parameter" "elasticache_endpoint" {
-  name  = "${var.env}.elasticache.endpoint"
-  type  = "String"
-  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
-}
+#resource "aws_elasticache_cluster" "elasticache" {
+#  cluster_id        = "${var.env}-elasticache"
+#  engine            = var.engine
+#  node_type         = var.node_type
+#  num_cache_nodes   = var.num_cache_nodes
+#  engine_version    = var.engine_version
+#  port              = 6379
+#  subnet_group_name = aws_elasticache_subnet_group.main.name
+#
+#  tags = merge(
+#    var.tags,
+#    { Name = "${var.env}-elasticache" }
+#  )
+#}
+#
+#resource "aws_elasticache_subnet_group" "main" {
+#  name       = "${var.env}-rds"
+#  subnet_ids = var.subnet_ids
+#
+#  tags = merge(
+#    var.tags,
+#    { Name = "${var.env}-subnet-group" }
+#  )
+#}
+#
+#output "redis" {
+#  value = aws_elasticache_cluster.elasticache
+#}
+#
+#resource "aws_ssm_parameter" "elasticache_endpoint" {
+#  name  = "${var.env}.elasticache.endpoint"
+#  type  = "String"
+#  value = aws_elasticache_cluster.elasticache.cache_nodes[0].address
+#}
